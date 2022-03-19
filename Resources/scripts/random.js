be65fb2b4c6f5c3c5c544c412f721005be58f655
MidniteOil <90993832+MidniteOil@users.noreply.github.com>
2022-03-18 21:11:31 -0600

    randomized spongebobs

--- Resources/scripts/random.js
+++ Resources/scripts/random.js
@@ -43,7 +43,12 @@ function getRandNum() {
     const max = parseInt( $1('#max-num-input').value );
     if (min && max) {
         const randNum = Math.floor(Math.random() * (max - min) + min);
-        $1('#random-num-output').innerHTML = randNum.toString();
+        $1('#random-num-output').innerHTML = "<h2 class='col-12'>" + randNum.toString() + " Spongebobs!</h2>";
+        let spongebob = '<br /> <img class="mx-auto d-block row col" style="padding: 15px;"src="https://thumbs.gfycat.com/SnarlingCreativeEnglishsetter-size_restricted.gif" />';
+
+        for (let i = 0; i < randNum; i++) {
+            $1('#random-num-output').innerHTML += spongebob;
+        }
     }
 
 }
